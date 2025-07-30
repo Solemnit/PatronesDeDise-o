6871db5caf985f8c68b3037bfac8e3ca00255218
Solemnit <santiagoceballosgutierres@gmail.com>
2025-07-29 21:17:07 -0600

    Saltar y Disparar
    Añadí el comando de saltar y dispara y a su y le di un valor a las teclas W y F para que al presionarlas se llame estas acciones (W=Saltar/ F= Disparar)

--- PatronesDeDiseñoCPP/Command.h
+++ PatronesDeDiseñoCPP/Command.h
@@ -1,8 +1,30 @@
 #pragma once
-class Command
+#include "Personaje.h"
+
+class ICommand
 {
 public:
-	Command();
-	~Command();
+    virtual ~ICommand();
+    virtual void execute(Personaje& personaje) = 0; //Quiere decir que no tiene implementación propia, se debe hacer en la clase que esta herede
+};
+
+// Comando concreto para la acción de Saltar
+class SaltarCommand : public ICommand
+{
+public:
+    virtual void execute(Personaje& personaje) override
+    {
+        personaje.Saltar();
+    }
+};
+
+// Comando concreto para la acción de Disparar
+class DispararCommand : public ICommand
+{
+public:
+    virtual void execute(Personaje& personaje) override
+    {
+        personaje.Disparar(); // Llama a la acción específica en el personaje
+    }
 };
 
--- PatronesDeDiseñoCPP/InputHandler.h
+++ PatronesDeDiseñoCPP/InputHandler.h
@@ -4,5 +4,11 @@ class InputHandler
 public:
 	InputHandler();
 	~InputHandler();
+
+	ICommand* handleInput(char tecla);
+
+private:
+	ICommand* botonW_;
+	ICommand* botonF_;
 };
 
